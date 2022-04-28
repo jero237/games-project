e91af8de971679e26426213e449d7bd2332b0799
jero237 <jeronimobalestra@gmail.com>
2022-04-28 19:21:39 -0300

    changed api rest url to remote

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const URL = "https://videogames-1.herokuapp.com/"
+const URL = "https://videogames-1.herokuapp.com"
 
 export const GET_ALL_GAMES = "GET_ALL_GAMES"
 export const SEARCH_GAMES = "SEARCH_GAMES"
