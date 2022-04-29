bd6eb755904b7ae71d1fd1713e6def2405748107
jero237 <jeronimobalestra@gmail.com>
2022-04-29 18:37:13 -0300

    Fixed nav logo padding

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 import styles from "./Nav.module.css"
 import logo from "../../resources/logo.png"
 import { useDispatch } from 'react-redux'
@@ -7,16 +7,19 @@ import { resetFilters, setPage } from '../../redux/actions'
 
 function Nav(props) {
 
+    const history = useHistory()
+
     const dispatch = useDispatch()
     const handleLogoClick = () => {
         dispatch(resetFilters())
         dispatch(setPage(1))
+        history.push("/main")
     }
 
 
     return (
-        <nav>
-            <Link to="/main" onClick={handleLogoClick}><img src={logo} alt="" className={styles.logo} /></Link>
+        <nav className={styles.nav}>
+            <img src={logo} alt="" className={styles.logo} onClick={handleLogoClick} />
             <Link to="/main/create-game" className={styles.link}>Add Game</Link>
         </nav>
     )
